f81ae12259ff902a322eafc55e722500e238c2fa
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-01-29 17:01:01 +0100

    add fetchs, homePage

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,2 +1,31 @@
-// const KEY = '7f02ea25314c53c27ad69245e14e3296';
-// 'https://api.themoviedb.org'
+import axios from 'axios';
+
+const KEY = 'fd2e32963129124006e8f776a5bbabab';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+export const fetchTrendingMovies = async () => {
+  const response = await axios.get(`/trending/movie/day?api_key=${KEY}`);
+  return response.data;
+};
+
+export const fetchSearchMovies = async query => {
+  const response = await axios.get(
+    `/search/movie?api_key=${KEY}&query=${query}`
+  );
+  return response.data;
+};
+
+export const fetchMoviesDetails = async movie_id => {
+  const response = await axios.get(`/movie/${movie_id}?api_key=${KEY}`);
+  return response.data;
+};
+
+export const fetchMoviesCast = async movie_id => {
+  const response = await axios.get(`/movie/${movie_id}/credits?api_key=${KEY}`);
+  return response.data;
+};
+export const fetchMoviesReviews = async movie_id => {
+  const response = await axios.get(`/movie/${movie_id}/reviews?api_key=${KEY}`);
+  return response.data;
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from 'react';
+import { fetchTrendingMovies } from '../components/Api';
+
 export default function HomePage() {
-  return <div>Home</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    const getTrendingMovies = async () => {
+      setIsLoading(true);
+      try {
+        const { results } = await fetchTrendingMovies();
+        setTrendingMovies(prevState => [...prevState, ...results]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getTrendingMovies();
+  }, []);
+
+  return (
+    <div>
+      <h2>Trending today</h2>
+      {isLoading && <p>Loading...</p>}
+      {error && (
+        <p>Oops, something went wrong... Please, try reloading this page!</p>
+      )}
+      <ul>
+        {trendingMovies.length > 0 &&
+          trendingMovies.map((trendingMovie, index) => {
+            const { title } = trendingMovie;
+            return <li key={index}>{title}</li>;
+          })}
+      </ul>
+    </div>
+  );
 }
