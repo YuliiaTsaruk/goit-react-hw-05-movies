286ae30c81526105c470e037364f50b9faa8506e
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-01-31 10:00:01 +0000

    Deploying to gh-pages from @ YuliiaTsaruk/goit-react-hw-05-movies@9acfb97f03750402f6f6fe0ebcb83bcdf63daaeb 🚀

---
+++ static/js/520.cb30e629.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,r,t){t.r(r),t.d(r,{default:function(){return u}});var n=t(87),o=t(184);function u(){return(0,o.jsx)("div",{children:(0,o.jsxs)("p",{children:["Sorry, page not found, but you can return to our"," ",(0,o.jsx)(n.rU,{to:"/",children:"Home page"})]})})}}}]);
+//# sourceMappingURL=520.cb30e629.chunk.js.map
