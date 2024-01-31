9bffaad60e2aeb32d388e09e9b099868dee0675b
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-01-31 09:28:00 +0000

    Deploying to gh-pages from @ YuliiaTsaruk/goit-react-hw-05-movies@ef4f7835a94635511bee826907cede91414e08e8 🚀

---
+++ static/js/520.4587d1a8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,t,n){function u(){}n.r(t),n.d(t,{default:function(){return u}})}}]);
+//# sourceMappingURL=520.4587d1a8.chunk.js.map
